2fdc9724bb4a03ec641db4b2cfacdd1e75cb9ce9
DavidOS10 <onatesanchezdavid@gmail.com>
2023-01-11 12:40:19 +0100

    atributos añadidos

--- src/witcher/TheWitcher.java
+++ src/witcher/TheWitcher.java
@@ -1,12 +1,13 @@
 package witcher;
 
 public class TheWitcher {
+	private String name;
+	private int health;
+	private boolean hasSoul;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
 		
 	}
 
